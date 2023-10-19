4e1720ef44329066716ad00d1227101c19830b6a
maksymopanasenko <maxpanas008@gmail.com>
2023-10-19 18:07:39 +0200

    change to whatsapp

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,7 @@ function App() {
   const [isOpen, setIsOpen] = useState(true);
   const [isCopied, setIsCopied] = useState(false);
 
-  function shareOnFacebook(url) {
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
-  }
-
-  function shareOnTelefram(url) {
-    window.open(`https://t.me/share/url?url=${encodeURIComponent(url)}`, '_blank');
-  }
-  function shareOnGmail(url) {
-    window.open('https://mail.google.com/mail/?view=cm&fs=1&to=&su=Check%20out%20this%20link!&body=Hey,%20I%20found%20this%20interesting%20link:%20' + encodeURIComponent(url), '_blank');
-  }
-  function shareOnViber(url) {
-    window.open("viber://forward?text=" + encodeURIComponent(url), '_blank');
-  }
-
-  const currentUrl = "https://chat.openai.com";
-  // const currentUrl = window.location.href;
+  const currentUrl = window.location.href;
 
   const copyToClipboard = () => {
     if (isCopied) return;
@@ -44,12 +29,14 @@ function App() {
       {isOpen && createPortal(
         <div className='overlay'>
           <div className="modal">
-            <p onClick={() => shareOnViber(currentUrl)}>Some link VB</p>
-            <p onClick={() => shareOnGmail(currentUrl)}>Some link GM</p>
-            <p onClick={() => shareOnFacebook(currentUrl)}>Some link FB</p>
-            <p onClick={() => shareOnTelefram(currentUrl)}>Some link TG</p>
-            <p onClick={copyToClipboard}>Скопіювати посилання</p>
-            {isCopied && <p>Link copied to clipboard!</p>}
+            <div className='links'>
+              <a href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentUrl)}`}>Facebook Link</a>
+              <a href={`https://t.me/share/url?url=${encodeURIComponent(currentUrl)}`}>Telegram Link</a>
+              <a href={`https://wa.me/?text=${encodeURIComponent(currentUrl)}`}>WhatsApp Link</a>
+              <a href={`https://mail.google.com/mail/?view=cm&fs=1&to=&su=Support%20out%20Armed%20Forces!&body=Hey,%20I%20found%20this%20interesting%20link:%20${encodeURIComponent(currentUrl)}`}>Gmail Link</a>
+            </div>
+            <button onClick={copyToClipboard}>Скопіювати посилання</button>
+            {isCopied && <div>Link copied to clipboard!</div>}
           </div>
         </div>,
         document.body
